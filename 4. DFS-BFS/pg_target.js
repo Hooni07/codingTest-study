11fc26739f80cfd3534d1c786f758da7aee7d047
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-02-19 17:12:04 +0900

    feat: re-config pg_target

--- 4. DFS-BFS/pg_target.js
+++ 4. DFS-BFS/pg_target.js
@@ -4,21 +4,9 @@
  */
 
 function solution(numbers, target) {
-  let node_num = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    node_num += 2 ** i;
-  }
-
-  let graph = [...new Array(node_num + 1)].map(() => []);
-  for (let i = 0; i < node_num - 1; i++) {
-    const start = Number(input[i + 1].split(" ")[0]);
-    const end = Number(input[i + 1].split(" ")[1]);
-    graph[start].push(end);
-    graph[end].push(start);
-  }
-  console.log(node_num);
   let answer = 0;
   return answer;
 }
 
-solution([1, 1, 1], 3);
+console.log(solution([1, 1, 1, 1, 1], 3));
+console.log(solution([4, 1, 2, 1], 4));
