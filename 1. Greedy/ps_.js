c4838c639a8f10631637cb85083c5e1dc7693a12
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-04-22 16:17:54 +0900

    Feat: solved(add) ps_큰 수 만들기

---
+++ 1. Greedy/ps_.js
@@ -0,0 +1,9 @@
+/*
+ * 프로그래머스 [큰 수 만들기] -> 공통 문제
+ * 2024.04.22.월
+ */
+
+function solution(number, k) {
+  let answer = "";
+  return answer;
+}
---
+++ 1. Greedy/ps_단속카메라.js
@@ -0,0 +1,32 @@
+/*
+ * 프로그래머스 [단속카메라] -> 공통 문제
+ * 2024.04.19.금
+ */
+
+function solution(routes) {
+  let answer = 1; // 첫 차량의 카메라 설치 여부를 확인하지 않은 case
+
+  // 진입 시점을 기준으로 오름차순 정렬
+  routes.sort((a, b) => a[0] - b[0]);
+
+  // 첫 진출시점(out)은 첫 차량의 진출시점으로 초기화
+  let out = routes[0][1];
+
+  for (let i = 1; i < routes.length; i++) {
+    // 진출시점보다 현재 차량의 진입이 느리다면
+    // 카메라 추가 설치 및 out 시점 갱신
+    if (out < routes[i][0]) {
+      answer++;
+      out = routes[i][1];
+    }
+
+    // 진출시점이 현재 차량의 진출시점보다 큰 경우
+    // 항상 out을 갱신해주어야
+    // 다음 차량 카메라 설치 여부 판별 가능
+    if (out > routes[i][1]) {
+      out = routes[i][1];
+    }
+  }
+
+  return answer;
+}
