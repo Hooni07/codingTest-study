b07b053bda721ecd52ffb9c8fcd5cda9792d16b2
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-05-07 09:30:01 +0900

    feat: solved common problems

--- ps 문제(0506)/boj_1302.js
+++ ps 문제(0506)/boj_1302.js
@@ -15,10 +15,15 @@ let maxCount = 0;
 let maxName = "";
 const dict = {};
 arr.forEach((name) => {
-  if (dict[name]) dict[name]++;
-  else dict[name] = 1;
-  if (dict[name] === maxCount && name < maxName) maxName = name;
-  else if (dict[name] > maxCount) {
+  if (dict[name]) {
+    dict[name]++;
+  } else {
+    dict[name] = 1;
+  }
+
+  if (dict[name] === maxCount && name < maxName) {
+    maxName = name;
+  } else if (dict[name] > maxCount) {
     maxName = name;
     maxCount = dict[name];
   }
