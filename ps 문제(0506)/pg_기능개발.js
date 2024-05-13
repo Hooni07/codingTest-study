c0efc1976d5daee27150e6289640b4306548de36
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-05-13 15:18:22 +0900

    Feat: solved 05/13 common problems

--- ps 문제(0506)/pg_기능개발.js
+++ ps 문제(0506)/pg_기능개발.js
@@ -5,5 +5,29 @@
 
 function solution(progresses, speeds) {
   let answer = [];
+  let day = 0;
+
+  while (progresses.length !== 0) {
+    let count = 0;
+    day++;
+    for (let i = 0; i < progresses.length; i++) {
+      progresses[i] += speeds[i];
+      if (i === 0) {
+        if (progresses[i] >= 100) {
+          count++;
+          progresses.splice(0, 1);
+          speeds.splice(0, 1);
+          i--;
+        }
+      }
+    }
+    if (count > 0) {
+      answer.push(count);
+    }
+  }
+
   return answer;
 }
+
+console.log(solution([93, 30, 55], [1, 30, 5]));
+console.log(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]));
