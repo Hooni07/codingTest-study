04b78d25c05e4157ee256a2423a139ffcb837199
Hooni07 <79882248+Hooni07@users.noreply.github.com>
2024-05-23 14:54:19 +0900

    feat: solved 05/23 problem

--- ps 문제(0506)/pg_이중우선순위큐.js
+++ ps 문제(0506)/pg_이중우선순위큐.js
@@ -5,14 +5,59 @@
 
 function solution(operations) {
   let answer = [];
-  let minValue = 0;
-  let maxValue = 0;
+  let minValue = Number.MAX_SAFE_INTEGER;
+  let maxValue = Number.MIN_SAFE_INTEGER;
   let queue = [];
   for (let i = 0; i < operations.length; i++) {
     const arr = operations[i].split(" ");
     const cmd = arr[0];
     const num = Number(arr[1]);
-    console.log(cmd, num);
+    let isDelete = false;
+
+    if (cmd === "I") {
+      queue.push(num);
+      queue.sort((a, b) => a - b);
+      if (queue[queue.length - 1] >= maxValue) {
+        maxValue = queue[queue.length - 1];
+      }
+      if (queue[0] <= minValue) {
+        minValue = queue[0];
+      }
+    } else if (cmd === "D") {
+      if (!isDelete) {
+        if (queue.length === 0) {
+          continue;
+        } else {
+          if (num === -1) {
+            queue.shift();
+            minValue = queue[0];
+          } else if (num === 1) {
+            queue.pop();
+            minValue = queue[queue.length - 1];
+          }
+          isDelete = true;
+          if (queue.length === 0) {
+            maxValue = Number.MIN_SAFE_INTEGER;
+            minValue = Number.MAX_SAFE_INTEGER;
+          } else if (queue.length === 1) {
+            maxValue = queue[0];
+            minValue = queue[0];
+          }
+        }
+      }
+    }
+  }
+
+  if (maxValue === Number.MIN_SAFE_INTEGER) {
+    answer.push(0);
+  } else {
+    answer.push(maxValue);
+  }
+
+  if (minValue === Number.MAX_SAFE_INTEGER) {
+    answer.push(0);
+  } else {
+    answer.push(minValue);
   }
 
   return answer;
